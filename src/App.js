812ab253a4fac2f73178688a060385261016076b
the-halfbloodprince <ammanishkumardas@gmail.com>
2021-01-31 00:34:11 +0530

    cleaned

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Row from "./Row";
+import Row from "./components/Row";
 import requests from "./requests";
-import Banner from "./Banner";
-import Nav from "./Nav";
+import Banner from "./components/Banner";
+import Nav from "./components/Nav";
 
 import './App.css';
 
